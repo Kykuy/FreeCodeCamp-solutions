877c4fbdf22a2cfdc46f7909f247caefa6315fd7
Kykuy <41070701+Kykuy@users.noreply.github.com>
2020-08-27 22:54:54 +0300

    Added solution to Sum All Odd Fibonacci Numbers

---
+++ JS Algorithms and Data Structures/Intermediate Algorithm Scripting/Sum All Odd Fibonacci Numbers.js
@@ -0,0 +1,15 @@
+function sumFibs(num) {
+  let fibNums = [1, 1];
+  
+  for (let i = 1; i <= num; i++) {
+    if (fibNums[i] + fibNums[i - 1] <= num) {
+      fibNums.push(fibNums[i] + fibNums[i - 1]);
+    } else break;
+  }
+  
+  return fibNums
+  .filter( item => item % 2 != 0)
+  .reduce( (accumulator, item) => accumulator + item, 0 );
+}
+
+sumFibs(44);
